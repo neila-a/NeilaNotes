e02ef706d26f3cf7e54285efeda5c709ba67ec95
neila-a <neilaspace@outlook.com>
2022-10-30 11:12:37 +0800

    fix:所有 修复没有return导致页面一片可能空白

--- pages/login.js
+++ pages/login.js
@@ -6,17 +6,19 @@ import PassWordEdit from "../components/PassWordEdit";
 import * as React from "react";
 import Head from "next/head";
 const loginPage = () => {
-    <Container maxWidth="sm" style={{
-        alignItems: "center",
-        justifyContent: "center",
-        textAlign: "center",
-        height: "100%"
-    }}>
-        <Head>
-            <title>登录</title>
-        </Head>
-        <Typography variant="h1">登录</Typography>
-        <PassWordEdit go="/main" />
-    </Container>
+    return (
+        <Container maxWidth="sm" style={{
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            height: "100%"
+        }}>
+            <Head>
+                <title>登录</title>
+            </Head>
+            <Typography variant="h1">登录</Typography>
+            <PassWordEdit go="/main" />
+        </Container>
+    );
 };
 export default loginPage;
--- pages/new.js
+++ pages/new.js
@@ -1,4 +1,4 @@
 const newPage = () => {
-    
+    return;
 };
 export default newPage;
