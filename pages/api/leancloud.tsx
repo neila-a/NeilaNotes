08ec5e4145cc0ea38e199f162ae373ad66a02a88
neila-a <neilaspace@outlook.com>
2022-12-05 12:08:21 +0800

    修改LeanCloud.tsx API

--- pages/api/leancloud.tsx
+++ pages/api/leancloud.tsx
@@ -1,72 +1,26 @@
-import AV from "leancloud-storage";
-import error from "../../functions/error";
-import Router from "next/router";
-import * as React from "react";
-declare global {
-    interface Window {
-        registerStatus: any[];
-        loginStatus: any[];
-        foundNotes: AV.Queriable[];
-    }
-}
-const { Query, User } = AV;
-AV.init({
-    appId: "LfguhHqPbp8hn5J4KUNkKQ8u-MdYXbMMI",
-    appKey: "nQkWznOWSYvE7N7TKrZ28iiv",
-    serverURL: "https://lfguhhqp.api.lncldglobal.com"
-});
-const regUser = (username, password, then) => {
-    var user = new User();
-    user.setUsername(username);
-    user.setPassword(password);
-    user.signUp().then((user) => {
-        // 注册成功
-        window.registerStatus = [
-            true,
-            user
-        ];
-        then();
-    }, (errInCall) => {
-        window.registerStatus =  error(errInCall, "注册");
-    })
-};
-const loginUser = (username, password, then) => {
-    AV.User.logIn(username, password).then((user) => {
-        // 登录成功
-        window.loginStatus =  [
-            true,
-            user
-        ];
-        then();
-    }, (errInCall) => {
-        window.loginStatus = error(errInCall, "登录");
-    })
-};
-const { logOut } = User;
-const bottomNote = AV.Object.extend("Note");
-class Note {
-    constructor(title, content, user) {
-        var noteInClass = new bottomNote();
-        noteInClass.set("title", title);
-        noteInClass.set("content", content);
-        noteInClass.set("author", user);
-        noteInClass.save();
-    }
-}
-const findNote = (author) => {
-    const query = new Query('Note');
-    query.equalTo('author', author);
-    query.find().then((notes) => {
-        window.foundNotes = notes;
-    });
-};
-export {
-    regUser,
-    regUser as registerUser,
-    loginUser,
-    logOut,
-    bottomNote,
-    Note,
-    findNote
-};
+import type { NextApiRequest, NextApiResponse } from 'next';
 
+export default (req: NextApiRequest, res: NextApiResponse) => {
+    switch (req.query.do) {
+        case "register":
+            res.status(200).json({
+                do: "register"
+            });
+            break;
+        case "login":
+            res.status(200).json({
+                do: "login"
+            });
+            break;
+        case "newNote":
+            res.status(200).json({
+                do: "newNote"
+            });
+            break;
+        case "deleteNote":
+            res.status(200).json({
+                do: "deleteNote"
+            });
+            break;
+    };
+};
